9e7fefc8e90e41c58eed5b629795327e1a9b5486
Joris <547363@student.saxion.nl>
2025-05-22 12:04:40 +0200

    Binary gap awnser

---
+++ BinaryGap/BinaryGap/Program.cs
@@ -0,0 +1,39 @@
+﻿
+
+namespace BinaryGap
+{
+    internal static class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new Solution();
+            var chain = solution.solution(15672897);
+            Console.WriteLine(chain);
+        }
+    }
+
+    internal class Solution
+    {
+        public int solution(int N)
+        {
+            var binary = Convert.ToString(N, 2);
+            List<int> binaryList = new List<int>();
+            int currentChain = 0;
+            Console.WriteLine(binary);
+            foreach (var c in binary)
+            {
+                if (c == '1')
+                {
+                    binaryList.Add(currentChain);
+                    currentChain = 0;
+                }
+                else 
+                    currentChain++;
+            }
+            binaryList.Add(currentChain);
+            binaryList.Sort();
+            binaryList.Reverse();
+            return binaryList[0];
+        }
+    }
+}
