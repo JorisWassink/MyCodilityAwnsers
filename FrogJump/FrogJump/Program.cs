3217a01721852ba4ee261a655601f65eaf60016e
Joris <wassinkjoris@gmail.com>
2025-05-26 14:57:43 +0200

    FrogJump awnser

---
+++ FrogJump/FrogJump/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace FrogJump
+{
+    internal static class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new Solution();
+            var jumpAmount = solution.solution(60, 500, 3);
+            Console.WriteLine(jumpAmount);
+        }
+    }
+    
+    internal class Solution {
+        public int solution(int X, int Y, int D) {
+            double s = Y - X;
+            double d = (s / D);
+            return (int)Math.Ceiling(d);
+        }
+    }
+    
+}
+
+
+
+
